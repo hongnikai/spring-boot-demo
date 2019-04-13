a90b5be6eba8faa2910c5de60dcd54324e10945b
master <541737239@qq.com>
2019-04-13 13:10:50 +0800

    创建一个git账户类

--- src/main/java/com/lc/bean/Git.java
+++ src/main/java/com/lc/bean/Git.java
@@ -15,4 +15,13 @@ public class Git {
     public void setName(String name) {
         this.name = name;
     }
+
+    //hah
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
