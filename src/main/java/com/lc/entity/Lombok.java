b80bb93af37d9f3a542488809a722ba3ad3b5a9a
lc <541737239@qq.com>
2019-05-13 10:56:43 +0800

    办公笔记本提交了 lombok 注解自动添加getter and setter

---
+++ src/main/java/com/lc/entity/Lombok.java
@@ -0,0 +1,17 @@
+package com.lc.entity;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@EqualsAndHashCode
+public class Lombok {
+
+    private String name;
+    private int age;
+
+
+}
